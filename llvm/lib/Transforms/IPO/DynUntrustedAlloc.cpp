0c7a53f015ae980baef68135b8401970ff1c4d25
Mitchel Dickerson <mgdicker@uci.edu>
2020-09-16 15:34:18 -0700

    Additional work on inlining Allocator functions.

--- llvm/lib/Transforms/IPO/DynUntrustedAlloc.cpp
+++ llvm/lib/Transforms/IPO/DynUntrustedAlloc.cpp
@@ -13,6 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/IPO/DynUntrustedAlloc.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Analysis/LazyCallGraph.h"
@@ -54,6 +55,31 @@ namespace
         return 0 == std::strncmp(name, TrustedAllocatorName, strlen(TrustedAllocatorName))
     }
 
+    class IDGenerator {
+        static unsigned long int id = 0;
+
+        public:
+            IDGenerator();
+
+            unsigned long int getID() {
+                return id++;
+            }
+    };
+
+    static IDGenerator IDG;
+
+    class UniqueID {
+        public:
+            const unsigned long int ID;
+            Function *F;
+            CallSite *CS;
+
+            // At a minimum I want to track CallSite information together with the Unique ID,
+            // but calling function F should be available as well and would provide useful debug info.
+            UniqueID(CallSite *CS, Function *F = nullptr, ID = IDG.getID()) : F(F), CS(CS), ID(ID) {}
+        private:
+    };
+
     class DynUntrustedAlloc : public ModulePass {
         public:
             static char ID;
@@ -89,6 +115,17 @@ namespace
                 &CG = getAnalysis<CallGraphWrapperPass>().getCallGraph();
             }
 
+            void mapCSID() {
+                ReversePostOrderTraversal<Function *> RPOT(CG);
+                for (rpo_iterator I = RPOT.begin(); I != RPOT.end(); ++I) {
+                    for (auto inst : *I) {
+                        // TODO : Figure out what I should be doing here. What data type is inst?? or I for that matter.
+                    }
+                }
+            }
+
+            
+
             void scanPostOrderGraph() {
                 for (auto GraphNode : post_order(&CG)) {
                     Function *F = GraphNode->getFunction();
@@ -105,28 +142,48 @@ namespace
                 }
             }
 
-            std::vector<Function*> getFunctionCallers(Function *F) {
+            std::vector<Function*> getFunctionRoot(Function *F) {
                 std::vector<Function*> WorkingSet {F};
-                // Loop over functions so long as 
-                while (!VisitedFunctions.insert(F).second) {
+                std::vector<Function*> ReturnSet;
 
-                }
-                auto FuncNode = CG[F];
-                for (auto CR : *FuncNode) {
-                    auto ParentNode = CR.second;
-                    if (!ParentNode) {
-                        continue;
+                // Loop over functions until last RustAllocator function is found.
+                while (!WorkingSet.empty()) {
+                    // Get Current Function, pop from WorkingSet
+                    Function *CF = WorkingSet.back();
+                    WorkingSet.pop_back();
+
+                    auto FuncNode = CG[F];
+                    for (auto CR : *FuncNode) {
+                        auto ParentNode = CR.second;
+                        if (!ParentNode) {
+                            continue;
+                        }
+
+                        // Get Parent Function of Current Function, check to see if it has the Rust Allocator Attribute
+                        Function *PF = ParentNode->getFunction();
+                        if PF->hasFnAttribute(Attribute::RustAllocator) {
+                            // PF has attribute, inline CF into parent and add PF to WorkingSet
+                            CallSite CS = CR.first;
+                            InlineFunctionInfo IFI(nullptr);
+
+                            if (!CS) {
+                                errs() << "Expected callsite for valid Parent Node Allocator.\n";
+                            }
+
+                            if (InlineFunction(CS, IFI).isSuccess()) {
+                                errs() << "Successfully inlined: " << CS.getCaller()->getName() << "\n";
+                            }
+
+                            WorkingSet.push_back(PF);
+                        } else {
+                            // Parent Function does not have RustAllocator attribute, so we add instrumentation here. 
+                            // Or we just add it to the ReturnSet and handle later.
+                            // TODO : Instrumentation.
+                        }
                     }
-
-                    auto ParentFunction = ParentNode->getFunction();
-                    if ParentFunction->hasFnAttribute(Attribute::RustAllocator) {
-                        return getAllocationRoot(ParentFunction);
-                    }
                 }
-            }
 
-            void dfs(CallGraphNode *Node) {
-
+                return ReturnSet;
             }
 
         private:
@@ -134,6 +191,7 @@ namespace
         LazyCallGraph &LCG = getAnalysis<LazyCallGraphAnalysis>().getGraph();
         std::set<Function *> VisitedFunctions;
         std::vector<Function *> GraphNodeVisitor;
+        DenseMap<CallSite *, UniqueID> CSMap;
         Function *hook;
-    }
+    };
 }
