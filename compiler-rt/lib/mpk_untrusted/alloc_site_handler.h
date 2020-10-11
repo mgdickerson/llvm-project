bda0219352d76f407279a3eb895b68952af6bc4e
Mitchel Dickerson <mgdicker@uci.edu>
2020-10-11 09:38:44 -0700

    Sync commit.

--- compiler-rt/lib/mpk_untrusted/alloc_site_handler.h
+++ compiler-rt/lib/mpk_untrusted/alloc_site_handler.h
@@ -3,17 +3,39 @@
 
 // TODO : Question: Can we assume that threaded requests for allocation will not overlap? Or will 
 // I need to make locks for inserting and retrieving allocations from AllocSiteHandler?
+
+// TODO : Answer for above, yes. We will need to write a simple spin lock.
+
+class AllocSite {
+    private:
+        int8_t* ptr;
+        int64_t size;
+        int64_t uniqueID;
+    public:
+        AllocSite(int8_t* ptr, int64_t size, int64_t uniqueID) : ptr(ptr), size(size), uniqueID(uniqueID) {}
+        
+        bool containsPtr(int8_t* ptrCmp) {
+            return (ptr <= ptrCmp) && (ptrCmp < (ptr + size));
+        }
+
+        int64_t uniqeID() {
+            return uniqeID;
+        }
+}
+
 class AllocSiteHandler {
     private:
         // Mapping from memory location pointer to AllocationSite
-        std::map<uintptr_t, void*> allocation_map;
-        void* ptr;
+        std::map<int8_t*, AllocSite> allocation_map;
     public:
-        AllocSiteHandler (void*);
-        void track_alloc(void* ptr, size_t size, size_t align);
-        void untrack_alloc(void* ptr, size_t size, size_t align);
-        void track_realloc(void* old_ptr, size_t old_size, void* new_ptr, size_t new_size, size_t align);
-        void allocHandlerHook(string funcName);
+        AllocSiteHandler();
+        void insertAllocSite(int8_t* ptr, AllocSite site);
+        void removeAllocSite(int8_t* ptr);
+        AllocSite getAllocSite(int8_t* ptr);
 };
 
-AllocSiteHandler GlobalHandler = AllocSiteHandler(nullptr);
+static AllocSiteHandler GlobalHandler = AllocSiteHandler(nullptr);
+
+void allocHook(int8_t* ptr, int64_t size, int64_t uniqueID);
+void reallocHook(int8_t* newPtr, int64_t newSize, int8_t* oldPtr, int64_t oldSize, int64_t uniqueID);
+void deallocHook(int8_t* ptr, int64_t size, int64_t uniqueID);
