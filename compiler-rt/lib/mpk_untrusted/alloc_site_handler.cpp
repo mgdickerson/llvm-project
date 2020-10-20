35e3fdc14279a144066e34104eb2f8e557e1f14a
Paul Kirth <pk1574@gmail.com>
2020-10-20 17:41:40 +0000

    Add basic logging to instrumentation hooks.
    TODO: these should probably be disabled after testing

--- compiler-rt/lib/mpk_untrusted/alloc_site_handler.cpp
+++ compiler-rt/lib/mpk_untrusted/alloc_site_handler.cpp
@@ -1,24 +1,34 @@
 #include "alloc_site_handler.h"
+#include "sanitizer_common/sanitizer_common.h"
 
-AllocSiteHandler* AllocSiteHandler::handle = nullptr;
+AllocSiteHandler *AllocSiteHandler::handle = nullptr;
 
 void allocHook(rust_ptr ptr, int64_t size, int64_t uniqueID) {
-    AllocSite site = AllocSite(ptr, size, uniqueID);
-    
-    auto handler = AllocSiteHandler::init();
-    (*handler)->insertAllocSite(ptr, site);
+  AllocSite site = AllocSite(ptr, size, uniqueID);
+  auto handler = AllocSiteHandler::init();
+  (*handler)->insertAllocSite(ptr, site);
+  __sanitizer::Report("INFO : AllocSiteHook for address: %p ID: %d.\n", ptr,
+                      uniqueID);
 }
 
-// TODO : For future versions, we want to union the previous AllocSite from old ptr, with the new ptr.
-void reallocHook(rust_ptr newPtr, int64_t newSize, rust_ptr oldPtr, int64_t oldSize, int64_t uniqueID) {
-    // TODO : For now we are simply going to remove the old one and input the new one.
-    auto handler = AllocSiteHandler::init();
-    (*handler)->removeAllocSite(oldPtr);
-    AllocSite site = AllocSite(newPtr, newSize, uniqueID);
-    (*handler)->insertAllocSite(newPtr, site);
+// TODO : For future versions, we want to union the previous AllocSite from old
+// ptr, with the new ptr.
+void reallocHook(rust_ptr newPtr, int64_t newSize, rust_ptr oldPtr,
+                 int64_t oldSize, int64_t uniqueID) {
+  // TODO : For now we are simply going to remove the old one and input the new
+  // one.
+  auto handler = AllocSiteHandler::init();
+  (*handler)->removeAllocSite(oldPtr);
+  AllocSite site = AllocSite(newPtr, newSize, uniqueID);
+  (*handler)->insertAllocSite(newPtr, site);
+  __sanitizer::Report(
+      "INFO : ReallocSiteHook for oldptr: %p, newptr: %p, ID: %d.\n", oldPtr,
+      newPtr, uniqueID);
 }
 
 void deallocHook(rust_ptr ptr, int64_t size, int64_t uniqueID) {
-    auto handler = AllocSiteHandler::init();
-    (*handler)->removeAllocSite(ptr);
+  auto handler = AllocSiteHandler::init();
+  (*handler)->removeAllocSite(ptr);
+  __sanitizer::Report("INFO : DeallocSiteHook for address: %p ID: %d.\n", ptr,
+                      uniqueID);
 }
