52d218d8dfa627dc8c59bdd9ea4bcb283d9cfb7f
Paul Kirth <pk1574@gmail.com>
2020-10-29 23:46:05 +0000

    Remove calls to copy constructor

--- compiler-rt/lib/mpk_untrusted/alloc_site_handler.cpp
+++ compiler-rt/lib/mpk_untrusted/alloc_site_handler.cpp
@@ -6,7 +6,7 @@ namespace __mpk_untrusted {
 std::shared_ptr<AllocSiteHandler> AllocSiteHandler::handle = nullptr;
 
 void allocHook(rust_ptr ptr, int64_t size, int64_t uniqueID) {
-  AllocSite site = AllocSite(ptr, size, uniqueID);
+  AllocSite site(ptr, size, uniqueID);
   auto handler = AllocSiteHandler::init();
   handler->insertAllocSite(ptr, site);
   __sanitizer::Report("INFO : AllocSiteHook for address: %p ID: %d.\n", ptr,
@@ -21,7 +21,7 @@ void reallocHook(rust_ptr newPtr, int64_t newSize, rust_ptr oldPtr,
   // one.
   auto handler = AllocSiteHandler::init();
   handler->removeAllocSite(oldPtr);
-  AllocSite site = AllocSite(newPtr, newSize, uniqueID);
+  AllocSite site(newPtr, newSize, uniqueID);
   handler->insertAllocSite(newPtr, site);
   __sanitizer::Report(
       "INFO : ReallocSiteHook for oldptr: %p, newptr: %p, ID: %d.\n", oldPtr,
@@ -35,4 +35,4 @@ void deallocHook(rust_ptr ptr, int64_t size, int64_t uniqueID) {
                       uniqueID);
 }
 
-} // namespace mpk
+} // namespace __mpk_untrusted
