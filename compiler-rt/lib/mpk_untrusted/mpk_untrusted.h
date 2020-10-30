3e7e291e41b4083a6460304bc6b4ffd90015a4df
Paul Kirth <pk1574@gmail.com>
2020-10-30 00:47:34 +0000

    Reformat test code and header file

--- compiler-rt/lib/mpk_untrusted/mpk_untrusted.h
+++ compiler-rt/lib/mpk_untrusted/mpk_untrusted.h
@@ -1,10 +1,12 @@
 #ifndef MPKUNTRUSTED_H
 #define MPKUNTRUSTED_H
 
-
 extern "C" {
+
 __attribute__((visibility("default"))) static void mpk_untrusted_constructor();
+
 __attribute__((visibility("default"))) static void __attribute__((constructor))
+
 mpk_untrusted();
 }
 
